42e3a770e8d08d88bbb731597385275f255cee91
mfghir <fatemeghafari77@gmail.com>
2024-07-13 19:10:16 +0330

    1. add shadcn + theme

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import { Button } from './ui/button'
+
+const HomePage = () => {
+	return (
+		<div>
+
+
+<Button variant="default">Button</Button>
+
+		</div>
+	)
+}
+
+export default HomePage
