79e9fd4e3a87d7175c965d7b88f135ee74e120be
mfghir <fatemeghafari77@gmail.com>
2024-07-29 20:21:44 +0330

    29. work on BlogDetailsPage 1

--- src/components/pages/BlogDetailsPage.tsx
+++ src/components/pages/BlogDetailsPage.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+"use client"
+
+import { blogPosts } from "@/utilities/types-data"
+import Image from "next/image"
+import { useParams, useRouter } from "next/navigation"
 
 const BlogDetailsPage = () => {
+  const { blogId } = useParams()
+  const postId = parseInt(blogId)
+  const post = blogPosts.find(post => post.id === postId)
+
+
   return (
-    <div>BlogDetailsPage</div>
+    <section className="w-full h-full place-items-start content-between grid grid-cols-1 gap-y-6 lg:grid-cols-2">
+
+      <div className="">
+        <h1 className="text-2xl text-gray-900 dark:text-white">
+          {post?.title}
+        </h1>
+
+        <p className=""></p>
+      </div>
+
+
+      <Image
+        className="lg:max-w-[550px] lg:max-h-[550px] w-fit h-fit object-fill rounded-3xl place-self-end self-start"
+        width={1024}
+        height={1024}
+        src={post?.articleImg}
+        alt="blog pic"
+      />
+    </section>
   )
 }
 
