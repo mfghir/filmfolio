8a90a3992ce50603b1b9e35c1ca144a4271a3a8e
mfghir <fatemeghafari77@gmail.com>
2024-08-24 17:26:06 +0330

    57. work one mongo error 6 + work on TopDetailUserPage 2

--- src/components/pages/TopDetailUserPage.tsx
+++ src/components/pages/TopDetailUserPage.tsx
@@ -6,7 +6,8 @@ import { usersList } from "@/utilities/types-data"
 
 import { Skeleton } from "../ui/skeleton"
 import { Button } from "../ui/button"
-import { IconCarambola, IconMessageCircle, IconMovie } from "@tabler/icons-react"
+import { IconCarambola, IconMessageCircle, IconMovie, IconPlus, IconUsersGroup, IconUsersPlus } from "@tabler/icons-react"
+
 
 
 const TopDetailUserPage = () => {
@@ -18,13 +19,13 @@ const TopDetailUserPage = () => {
 
   return (
     <>
-      <section className="my-12 grid lg:grid-cols-3 lg:gap-x-12">
-        <div className="flex flex-row items-center gap-x-8 ">
+      <section className="my-12 grid gap-y-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-12 bg-gray-200 dark:bg-gray-800 py-6 md:p-6 rounded-xl items-center">
+        <div className="flex flex-col md:flex-row justify-center md:justify-normal items-center gap-x-8 gap-y-3 md:gap-y-0 md:col-span-2 self-center">
           {!user?.userPic ?
             <Skeleton className="h-full w-full" />
             :
             <Image
-              className=" md:max-w-[170px] md:max-h-[170px] w-fit h-fit object-fill rounded-full "
+              className="w-28 h-28 md:max-w-[170px] md:max-h-[170px] object-fill rounded-full "
               width={1024}
               height={1024}
               src={user?.userPic ? user?.userPic : "https://i.postimg.cc/rpN1DtvM/uer-pic.jpg"}
@@ -33,11 +34,10 @@ const TopDetailUserPage = () => {
           }
 
 
-
           <ul className="flex flex-col gap-y-2">
             <li className="text-xl font-bold">{user?.userName}</li>
-            <li className="text-base text-gray-500">{user?.userAge}</li>
-            <li className="text-base text-gray-500">{user?.userGenre}</li>
+            <li className="text-base text-gray-500">{user?.userAge} ساله</li>
+            <li className="text-base text-gray-500">طرفدار {user?.userGenre}</li>
             <li className="text-base text-gray-500">{user?.userEducation}</li>
           </ul>
         </div>
@@ -45,21 +45,20 @@ const TopDetailUserPage = () => {
 
 
 
-        <div className=" flex flex-col gap-y-6 mx-auto">
+        <div className=" flex flex-col items-center gap-y-6 mx-auto">
           <ul className="flex flex-row gap-x-4">
             <li className="flex flex-col gap-y-3 items-center">
-              <p className="flex flex-row  items-center gap-x-2">
-                <IconCarambola stroke={1.5} color="" />
+              <p className="flex flex-row flex-nowrap items-center gap-x-2">
+                <IconCarambola stroke={1.5} color="#bfbfbf" />
                 <span className="text-lg font-bold">امتیاز</span>
               </p>
 
               <span className="text-sm">{user?.userScore}</span>
             </li>
 
-
             <li className="flex flex-col gap-y-3 items-center">
-              <p className="flex flex-row  items-center gap-x-2">
-                <IconMessageCircle stroke={1.5} />
+              <p className="flex flex-row flex-nowrap items-center gap-x-2">
+                <IconMessageCircle stroke={1.5} color="#bfbfbf" />
                 <span className="text-lg font-bold">نظر ها</span>
               </p>
 
@@ -67,8 +66,8 @@ const TopDetailUserPage = () => {
             </li>
 
             <li className="flex flex-col gap-y-3 items-center">
-              <p className="flex flex-row  items-center gap-x-2">
-                <IconMovie stroke={1.5} />
+              <p className="flex flex-row flex-nowrap items-center gap-x-2">
+                <IconMovie stroke={1.5} color="#bfbfbf" />
                 <span className="text-lg font-bold">فیلم ها</span>
               </p>
 
@@ -86,15 +85,40 @@ const TopDetailUserPage = () => {
 
 
 
+        <div className=" flex flex-col gap-y-4 lg:gap-y-6 mx-auto">
+          <p className="text-lg font-bold text-center">ID: {user?.userId}</p>
+
+          <ul className="flex flex-col gap-y-4 bg-red-500">
+            <li className="flex flex-row justify-between items-center">
+              <p className="flex flex-row  items-center gap-x-1">
+
+                <IconUsersGroup stroke={1.5} color="#bfbfbf" />
+                <span className="text-lg font-bold">دنبال شونده</span>
+              </p>
+
+              <span className="text-sm">{user?.followers}</span>
+            </li>
+
+            <li className="flex flex-row justify-between items-center">
+              <p className="flex flex-row items-center gap-x-1">
+
+                <IconUsersPlus stroke={1.5} color="#bfbfbf" />
+                <span className="text-lg font-bold">دنبال کننده</span>
+              </p>
+
+              <span className="text-sm">{user?.followings}</span>
+            </li>
+          </ul>
 
 
-
-
-
-        <div className="">
-          sdds
+          <Button
+            variant="default"
+            className="w-fit py-2 px-12 font-semibold text-base text-white galaxy-gradient-bg"
+          >
+            <IconPlus stroke={1.5} />
+            دنبال کردن
+          </Button>
         </div>
-
       </section>
 
 
